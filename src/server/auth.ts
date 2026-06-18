7debe69e1372a32b0ab7a4a47b45cfdda9b8a818
Zahid2297 <techpirate97@gmail.com>
2026-06-18 18:55:58 +0530

    add auth credenstials and sonfiger it

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,9 +1,22 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "./db"; // your drizzle instance
+import { db } from "./db";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+
+  plugins: [nextCookies()],
 });
